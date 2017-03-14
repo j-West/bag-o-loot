45a350819b193003fecac493c1196b3f931f410a
j-west <westja89@gmail.com>
2017-03-14 18:08:03 -0500

    Update remove test file to add then remove a table entry before the test runs

--- test/remove.js
+++ test/remove.js
@@ -1,19 +1,21 @@
 
 const { assert } = require('chai');
 const remove = require('../lib/remove');
+const add = require('../lib/add');
 const showChild = require('../lib/show-child');
 
 describe('remove.js', () => {
 
   before(() => {
-    remove("james", "cat")
+    add("chris", "dog")
+    remove("chris", "dog")
   })
 
   it('should return an empty array', () => {
-    return showChild("james")
+    return showChild("chris")
       .then((rows) => {
         let row = rows.filter((rows) => {
-          return rows.child_toy === "cat"
+          return rows.child_toy === "dog"
         });
         console.log(row);
         assert.equal(row[0], null);
