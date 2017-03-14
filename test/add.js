ae558fdf773c431305878962d8f93a1a3f61c3d9
j-west <westja89@gmail.com>
2017-03-14 11:52:33 -0500

    Add empty files for future tests

---
+++ test/add.js
@@ -0,0 +1,9 @@
+// 
+// const { assert } = require('chai');
+// const { add } = require('../lib/add');
+//
+// describe('add.js', () => {
+//   it('should return a promise', () => {
+//     assert.
+//   })
+// })
